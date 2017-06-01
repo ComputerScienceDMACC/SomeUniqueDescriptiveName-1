9716e58fc3493ebc69c4343e8e0840461612aabd
900918686 <cbeh@dmacc.edu>
2017-06-01 14:35:43 -0500

    Cause you only need the light when it's burning low

--- TheBrain.java
+++ TheBrain.java
@@ -3,8 +3,7 @@
 public class TheBrain {
 
 	public static void main(String[] args) {
-		System.out.println("does it work?");
-		System.out.println("So far, so good...");
+		System.out.println("It works for me!");
 		// add more
 	}
 }
